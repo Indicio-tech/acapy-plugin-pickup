30e288a1f59981ac24fca2ca775a0dc94920a223
Micah Peltier <micah6_8@yahoo.com>
2021-11-04 14:53:04 -0600

    WIP: fixtures and imports
    Signed-off-by: Micah Peltier <micah6_8@yahoo.com>

--- int/tests/conftest.py
+++ int/tests/conftest.py
@@ -2,18 +2,44 @@
 
 import asyncio
 import pytest
-from typing import Union
+from typing import Union, Optional, Iterator
 import httpx
+import hashlib
+import os
 
-from acapy_client.models import ConnectionStaticResult
+# TODO: Remove debugging tools before final commit
+import logging
+
+from acapy_client import Client
+from acapy_client.models import (
+    ConnectionStaticResult,
+    ConnectionStaticRequest,
+    ConnectionMetadataSetRequest
+)
 from acapy_client.models.conn_record import ConnRecord
+from acapy_client.api.connection import (
+    create_static,
+    delete_connection,
+    set_metadata,
+)
 
-from aries_staticagent import StaticConnection, Target
+from aries_staticagent.connection import Connection as StaticConnection
 from aries_staticagent.message import Message
 
-from echo_agent_client import Client as EchoClient
-from echo_agent_client.models import Connection as EchoConnection
-from echo_agent_client.api.default import retrieve_messages
+from echo_agent.client import EchoClient
+from echo_agent.models import ConnectionInfo as EchoConnection
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    """Create a session scoped event loop.
+    pytest.asyncio plugin provides a default function scoped event loop
+    which cannot be used as a dependency to session scoped fixtures.
+    """
+    return asyncio.get_event_loop()
 
 
 class IntegrationTestConnection(StaticConnection):
@@ -36,39 +62,119 @@ class IntegrationTestConnection(StaticConnection):
 
 
 @pytest.fixture(scope="session")
-def asynchronously_received_messages(
-    echo_client: EchoClient, echo_connection: EchoConnection
-):
-    """Get asynchronously recevied messages from the echo agent."""
-    # Could wipe left over messages here
-    async def _asynchronously_received_messages(timeout: int = 5):
-        timed_client = echo_client.with_timeout(timeout)
-        try:
-            messages = await retrieve_messages.asyncio(
-                client=timed_client, connection_id=echo_connection.connection_id
-            )
-        except httpx.ReadTimeout:
-            raise Exception(
-                "Retrieving asynchronously recevied messages timed out"
-            ) from None
+def host():
+    """Hostname of agent under test."""
+    return os.environ.get("AGENT_HOST", "localhost")
 
-        return messages
 
-    yield _asynchronously_received_messages
-    # Could wipe remaining messages here
+@pytest.fixture(scope="session")
+def backchannel_port():
+    """Port of agent under test backchannel."""
+    return os.environ.get("AGENT_BACKCHANNEL_PORT", 3001)
+
+
+@pytest.fixture(scope="session")
+def backchannel(host, backchannel_port):
+    """Yield backchannel client."""
+    yield Client(base_url="http://{}:{}".format(host, backchannel_port))
+
+
+@pytest.fixture(scope="session")
+def suite_seed():
+    yield hashlib.sha256(b"acapy-plugin-toolbox-int-test-runner").hexdigest()[:32]
+
+
+@pytest.fixture(scope="session")
+def agent_seed():
+    yield hashlib.sha256(b"acapy-plugin-toolbox-int-test-runner").hexdigest()[:32]
+
+
+@pytest.fixture(scope="session")
+def suite_endpoint():
+    yield os.environ.get("SUITE_ENDPOINT", "http://localhost:3000")
+
+
+@pytest.fixture(scope="session")
+def port():
+    """Port of agent under test."""
+    return os.environ.get("AGENT_PORT", 3000)
+
+
+@pytest.fixture(scope="session")
+def echo_endpoint(host, port):
+    yield "http://{}:{}".format(host, port)
+
+
+@pytest.fixture(scope="session")
+def agent_connection(
+    suite_seed, agent_seed, suite_endpoint, backchannel
+) -> Iterator[ConnectionStaticResult]:
+    """Yield agent's representation of this connection."""
+
+    # Create connection in agent under test
+    create_result: Optional[ConnectionStaticResult] = create_static.sync(
+        client=backchannel,
+        json_body=ConnectionStaticRequest.from_dict(
+            {
+                "my_seed": agent_seed,
+                "their_seed": suite_seed,
+                "their_endpoint": suite_endpoint,
+                "their_label": "test-runner",
+            }
+        ),
+    )
+    if not create_result:
+        raise RuntimeError("Could not create static connection with agent under test")
+
+    # Set admin metadata to enable access to admin protocols
+    set_result = set_metadata.sync(
+        client=backchannel,
+        conn_id=create_result.record.connection_id,
+        json_body=ConnectionMetadataSetRequest.from_dict(
+            {"metadata": {"group": "admin"}}
+        ),
+    )
+    if not set_result:
+        raise RuntimeError("Could not set metadata on static connection")
+
+    yield create_result
+
+    delete_connection.sync(
+        client=backchannel, conn_id=create_result.record.connection_id
+    )
+
+
+@pytest.fixture(scope="session")
+def echo_agent(echo_endpoint: str):
+    yield EchoClient(base_url=echo_endpoint)
+
+
+@pytest.fixture
+async def echo(echo_agent: EchoClient):
+    async with echo_agent as client:
+        yield client
+
+
+@pytest.fixture(scope="session")
+def conn_record(agent_connection: ConnectionStaticResult):
+    yield agent_connection.record
+
 
 @pytest.fixture(scope="session")
 def connection_id(conn_record: ConnRecord):
     yield conn_record.connection_id
 
-@pytest.fixture(scope="session")
-def connection(agent_connection: ConnectionStaticResult, suite_seed: str):
-    """Yield static connection to agent under test."""
 
+@pytest.fixture(scope="session", autouse=True)
+async def connection(
+    agent_connection: ConnectionStaticResult, echo_agent: EchoClient, suite_seed: str
+):
+    """Yield static connection to agent under test."""
     # Create and yield static connection
-    yield IntegrationTestConnection.from_seed(
-        seed=suite_seed.encode("ascii"),
-        target=Target(
-            endpoint=agent_connection.my_endpoint, their_vk=agent_connection.my_verkey
-        ),
-    )
+    async with echo_agent as echo:
+        conn = await echo.new_connection(
+            seed=suite_seed,
+            endpoint=agent_connection.my_endpoint,
+            their_vk=agent_connection.my_verkey,
+        )
+    yield conn
--- int/tests/test_statusrequest.py
+++ int/tests/test_statusrequest.py
@@ -1,9 +1,38 @@
 """Status Request and response tests"""
 
 import asyncio
+from echo_agent.client import EchoClient
 import pytest
 from acapy_client import Client
 
+import logging
+LOGGER = logging.getLogger(__name__)
+
+@pytest.mark.asyncio
 def test_statusrequest():
 
     assert True
+
+
+@pytest.mark.asyncio
+async def test_send(
+    connection_id: str, echo: EchoClient
+):
+    """Test send message"""
+    await echo.send_message(
+        {
+            "@type": "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/send",
+            "connection_id": connection_id,
+            "content": "Your hovercraft is full of eels.",
+        }
+    )
+    [sent_message, recip_message] = await echo.get_messages()
+    assert (
+        sent_message["@type"]
+        == "https://github.com/hyperledger/aries-toolbox/tree/master/docs/admin-basicmessage/0.1/sent"
+    )
+    assert (
+        recip_message["@type"]
+        == "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/basicmessage/1.0/message"
+    )
+    assert recip_message["content"] == "Your hovercraft is full of eels."
