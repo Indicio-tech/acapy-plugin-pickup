8e4f84d87344afaa51931c83c9418e1bb26a2f09
Micah Peltier <micah6_8@yahoo.com>
2021-11-11 09:45:13 -0700

    feat: additional Delivery Request tests
    Signed-off-by: Micah Peltier <micah6_8@yahoo.com>

--- int/tests/test_deliveryrequest.py
+++ int/tests/test_deliveryrequest.py
@@ -1,9 +1,12 @@
+import re
+from attr import resolve_types
 from echo_agent.client import EchoClient
 from echo_agent.models import ConnectionInfo
 import pytest
 
 import logging
 LOGGER = logging.getLogger(__name__)
+
 
 @pytest.mark.asyncio
 async def test_delivery_request_empty_queue(
@@ -27,3 +30,34 @@ async def test_delivery_request_empty_queue(
         status["@type"]
         == "https://didcomm.org/messagepickup/2.0/status"
     )
+
+
+@pytest.mark.asyncio
+async def test_delivery_request_with_queued(
+    echo: EchoClient, connection: ConnectionInfo
+):
+    await echo.send_message(
+        connection,
+        {
+            "@type": "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/trust_ping/1.0/ping",
+                "response_resquested": True
+        }
+    )
+
+    await echo.send_message(
+        connection,
+        {
+            "@type": "https://didcomm.org/messagepickup/2.0/delivery-request",
+            "limit": 1,
+            "~transport": {
+                "return_route": "all"
+            }
+        }
+    )
+
+    response = await echo.get_message(connection)
+    assert (
+        response["@type"]
+        == "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/trust_ping/1.0/ping_response"
+    )
+
--- int/tests/test_placeholder.py
+++
@@ -1,5 +0,0 @@
-import asyncio
-import pytest
-
-def test_placeholder():
-    assert True
