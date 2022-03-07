bfa2d8738456eb031b8b41063eb4b465f35c45a3
Micah Peltier <micah6_8@yahoo.com>
2022-03-07 12:49:27 -0700

    feat: simple proof of concept handler for undeliverable event
    Signed-off-by: Micah Peltier <micah6_8@yahoo.com>

--- acapy_plugin_pickup/__init__.py
+++ acapy_plugin_pickup/__init__.py
@@ -1,13 +1,23 @@
 """ACA-Py Pickup Protocol Plugin."""
 
+import logging
+import re
+
 from aries_cloudagent.config.injection_context import InjectionContext
+from aries_cloudagent.core.event_bus import Event, EventBus
+from aries_cloudagent.core.profile import Profile
 from aries_cloudagent.core.protocol_registry import ProtocolRegistry
+
+from .protocol.delivery import Delivery, DeliveryRequest, MessagesReceived
 from .protocol.live_mode import LiveDeliveryChange
-from .protocol.delivery import DeliveryRequest, Delivery, MessagesReceived
-from .protocol.status import StatusRequest, Status
+from .protocol.status import Status, StatusRequest
+
+UNDELIVERABLE_EVENT_TOPIC = re.compile("acapy::outbound-message::undeliverable")
+LOGGER = logging.getLogger(__name__)
 
 
 async def setup(context: InjectionContext):
+    LOGGER.debug("Hit Pickup Plugin setup")
     """Setup plugin."""
     protocol_registry = context.inject(ProtocolRegistry)
     assert protocol_registry
@@ -20,4 +30,13 @@ async def setup(context: InjectionContext):
             MessagesReceived.message_type: MessagesReceived,
             LiveDeliveryChange.message_type: LiveDeliveryChange,
         }
+    )
+
+    event_bus = context.inject(EventBus)
+    event_bus.subscribe(UNDELIVERABLE_EVENT_TOPIC, undeliverable)
+
+
+async def undeliverable(profile: Profile, event: Event):
+    LOGGER.debug(
+        "Undeliverable Event Captured in Pickup Protocol: ", event.topic, event.payload
     )
