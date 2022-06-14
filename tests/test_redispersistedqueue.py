4a2dfba6bd286de13f45d8a5a52381051d555d58
Peter Strobel <peter@indicio.tech>
2022-06-14 09:57:36 -0700

    feat: unit test for RedisPersistedQueue class
    Signed-off-by: Peter Strobel <peter@indicio.tech>

--- tests/test_redispersistedqueue.py
+++ tests/test_redispersistedqueue.py
@@ -1,21 +1,34 @@
-import aioredis
+import fakeredis
 import pytest
 
 from acapy_plugin_pickup.protocol.delivery import RedisPersistedQueue
 from aries_cloudagent.transport.outbound.message import OutboundMessage
-from unittest.mock import Mock, patch
+from aries_cloudagent.connections.models.connection_target import ConnectionTarget
+
+
+"""
+{
+  "result": {
+    "did": "Sfrv1gcBQyqRy2EGfkZDek",
+    "verkey": "EzUpTawM8uLQfGW9pTt6HWVT6ZsTeFdgjxSU7WLTRCVm",
+    "posture": "wallet_only",
+    "key_type": "ed25519",
+    "method": "sov"
+  }
+}
+"""
 
 
 @pytest.fixture
 def target():
-    yield {        
-        "did": "some_did",
-        "endpoint": "some_endpoint",
-        "label": "some_label",
-        "recipient_keys": "recipient_keys",
-        "routing_keys": "routing_keys",
-        "sender_key": "some_sender_key",
-    }
+    yield ConnectionTarget(
+        did="Sfrv1gcBQyqRy2EGfkZDek",
+        endpoint="some_endpoint",
+        label="some_label",
+        recipient_keys=["EzUpTawM8uLQfGW9pTt6HWVT6ZsTeFdgjxSU7WLTRCVm"],
+        routing_keys=["EzUpTawM8uLQfGW9pTt6HWVT6ZsTeFdgjxSU7WLTRCVm"],
+        sender_key="EzUpTawM8uLQfGW9pTt6HWVT6ZsTeFdgjxSU7WLTRCVm",
+    )
 
 
 @pytest.fixture
@@ -23,6 +36,7 @@ def msg(target):
     yield OutboundMessage(
         connection_id="conn_id",
         target=target,
+        target_list=[],
         reply_from_verkey="reply_from_verkey",
         payload="payload",
     )
@@ -34,34 +48,35 @@ async def test_persistedqueue(msg):
     PersistedQueue Test.
     Unit test for the delivery protocol RedisPersistedQueue class.
     """
-    PQ = RedisPersistedQueue
-    key = msg.target["recipient_keys"]
+    PQ = RedisPersistedQueue(
+        redis=fakeredis.FakeStrictRedis(host="localhost", port=6379, db=0)
+    )
+    key = " ".join(msg.target.recipient_keys)
 
-    await PQ.queue_by_key.flushdb(key)
-    initial_queue = await PQ.queue_by_key.llen(key)
+    PQ.queue_by_key.flushall(key)
+    initial_queue = PQ.queue_by_key.llen(key)
 
-    await PQ.add_message(key, str(msg))
-    added_queue = await PQ.queue_by_key.llen(key)
+    await PQ.add_message(key, msg)
+    added_queue = PQ.queue_by_key.llen(key)
     assert added_queue == initial_queue + 1
 
     message_for_key = await PQ.has_message_for_key(key)
     assert message_for_key
 
     message_count = await PQ.message_count_for_key(key)
-    assert message_count == await PQ.queue_by_key.llen(key)
+    assert message_count == PQ.queue_by_key.llen(key)
 
     get_message_for_key = await PQ.get_one_message_for_key(key)
     assert str(get_message_for_key) == str(msg)
-    assert await PQ.queue_by_key.llen(key) == 0
+    assert PQ.queue_by_key.llen(key) == 0
 
-    await PQ.add_message(key, str(msg))
-    await PQ.add_message(key, str(msg))
-    new_added_queue_len = await PQ.queue_by_key.llen(key)
+    await PQ.add_message(key, msg)
+    await PQ.add_message(key, msg)
+    new_added_queue_len = PQ.queue_by_key.llen(key)
     inspect_messages = await PQ.inspect_all_messages_for_key(key)
     assert inspect_messages
     assert len(inspect_messages) == new_added_queue_len
 
     remove_message = await PQ.remove_message_for_key(key)
     assert remove_message
-    assert await PQ.queue_by_key.llen(key) == 1
-    
+    assert PQ.queue_by_key.llen(key) == 1
