2579e19733576f506bd7dc61d4893c356239df6c
Peter Strobel <peter@indicio.tech>
2022-07-06 10:11:13 -0700

    feat: updated redis queue unit tests
    Signed-off-by: Peter Strobel <peter@indicio.tech>

--- tests/test_redispersistedqueue.py
+++ tests/test_redispersistedqueue.py
@@ -1,12 +1,16 @@
+from email import message_from_string
 import json
+from pyexpat.errors import messages
 from typing import Any, Dict, List, Optional
+from acapy_plugin_pickup.acapy import message
 import pytest
 from unittest import mock
 from redis.asyncio import Redis
 
-from acapy_plugin_pickup.protocol.delivery import RedisPersistedQueue, msg_serialize
+from acapy_plugin_pickup.protocol.redis_queue import RedisPersistedQueue, msg_serialize
 from aries_cloudagent.transport.outbound.message import OutboundMessage
 from aries_cloudagent.connections.models.connection_target import ConnectionTarget
+from requests import delete
 
 
 @pytest.fixture
@@ -140,3 +144,103 @@ async def test_get_one_message_for_key(
     else:
         assert msg
         assert msg_serialize(expected) == msg_serialize(msg)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    ("msg_queue", "messages", "expected"),
+    [
+        ([], {}, []),
+        (
+            ["1", "2", "3"],
+            {"1": OutboundMessage(payload="1"), 
+            "2": OutboundMessage(payload="2"), 
+            "3": OutboundMessage(payload="3")},
+            [OutboundMessage(payload="1"), 
+            OutboundMessage(payload="2"), 
+            OutboundMessage(payload="3")]
+        ),
+    ],
+)
+async def test_inspect_all_messages_for_key(
+    queue: RedisPersistedQueue,
+    key: str,
+    mock_redis: mock.MagicMock,
+    monkeypatch: pytest.MonkeyPatch,
+    msg_queue: List[OutboundMessage],
+    messages: Dict[str, OutboundMessage],
+    expected: List[OutboundMessage]
+):
+    async def _mget(key: str):
+        return [json.dumps(msg_serialize(message)) for message in messages.values()]
+
+    monkeypatch.setattr(mock_redis, "lrange", CoroutineMock(return_value=msg_queue))
+    monkeypatch.setattr(mock_redis, "mget", _mget)
+
+    msgs = await queue.inspect_all_messages_for_key(key)
+    assert len(msgs) == len(expected)
+    for i in range(len(expected)):
+        assert msg_serialize(msgs[i]) == msg_serialize(expected[i])
+
+
+@pytest.mark.asyncio
+async def test_remove_message_for_key(
+    queue: RedisPersistedQueue,
+    key: str,
+    mock_redis: mock.MagicMock,
+    monkeypatch: pytest.MonkeyPatch,
+    msg: OutboundMessage
+):
+    async def _lrem(key:str, count:int, msg_key):
+        if key not in messages or messages[key] is None:
+            return None
+        messages_left = count
+        while messages_left != 0:
+            for message in messages[key]:
+                if msg_key in message:
+                    messages[key].pop(message)
+            messages_left -= 1
+        return count
+
+    monkeypatch.setattr(mock_redis, "lrem", _lrem)
+    del_mock = CoroutineMock()
+    monkeypatch.setattr(mock_redis, "delete", del_mock)
+    monkeypatch.setattr(mock_redis, "lrange", CoroutineMock())
+
+    await queue.remove_message_for_key(key, msg)
+    del_mock.assert_called_once()
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    ("msg_queue", "message_list"),
+    [
+        (
+            ["1", "2", "3"],
+            [OutboundMessage(payload="1"), 
+            OutboundMessage(payload="2"), 
+            OutboundMessage(payload="3")]
+        ),
+    ],
+)
+async def test_flush_messages(
+    queue: RedisPersistedQueue,
+    key: str,
+    mock_redis: mock.MagicMock,
+    monkeypatch: pytest.MonkeyPatch,
+    msg_queue: List[OutboundMessage],
+    message_list: List[OutboundMessage]
+):
+    monkeypatch.setattr(
+        RedisPersistedQueue, 
+        "inspect_all_messages_for_key", 
+        CoroutineMock(return_value=message_list)
+        )
+    monkeypatch.setattr(mock_redis, "lrange", CoroutineMock(return_value=msg_queue))
+    del_mock = CoroutineMock()
+    monkeypatch.setattr(mock_redis, "delete", del_mock)
+
+    flushed = await queue.flush_messages(key)
+
+    assert flushed == message_list
+    del_mock.assert_called_once_with(*msg_queue, key)
